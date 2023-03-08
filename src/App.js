fffe2aa57e5b48367dfc1b0982653580a6159056
Isaac Idibia <idibiaisaac3@gmail.com>
2023-03-08 23:20:57 +0100

    useQuery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { HomePage } from "./components/Home.page";
+import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
+import { SuperHeroesPage } from "./components/SuperHeroes.page";
+import RQSuperHero from "./components/RQSuperHero.page";
+import ParallelQueries from "./components/ParallelQueries.page";
+import DynamicParallel from "./components/DynamicParallel.page";
+import DependentQueries from "./components/DependentQueries.page";
+import InfiniteQueries from "./components/InfiniteQueries.page";
+import PaginatedQueries from "./components/PaginatedQueries.page";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/super-heroes" element={<SuperHeroesPage />} />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+            <Route path="/rq-parallel" element={<ParallelQueries />} />
+            <Route path="/rq-infinite" element={<InfiniteQueries />} />
+            <Route path="/rq-paginated" element={<PaginatedQueries />} />
+            <Route
+              path="/rq-dependent"
+              element={<DependentQueries email="isaac@example.com" />}
+            />
+            <Route
+              path="/rq-dynamic-parallel"
+              element={<DynamicParallel heroIds={[1, 3]} />}
+            />
+            <Route path="/rq-super-heroes/:heroId" element={<RQSuperHero />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import useSuperHeroesData from "../hooks/useSuperHeroesData";
+
+export const RQSuperHeroesPage = () => {
+  const onSuccess = () => {
+    console.log("Performs side effect after data fetching");
+  };
+
+  const onError = () => {
+    console.log("perform side effect after encountering error");
+  };
+
+  const { isLoading, data, isError, error, isFetching, refetch } =
+    useSuperHeroesData(onSuccess, onError);
+
+  if (isLoading || isFetching) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
+      {data?.data.map((hero) => {
+        return (
+          <div key={hero.id}>
+            <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+          </div>
+        );
+      })}
+      {/* {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>;
+      })} */}
+    </>
+  );
+};
